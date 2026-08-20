a3a759c1befaebb03cf96696da71d1e0d51d48ee
Gianni <gianni@example.com>
2026-08-20 19:19:51 +0200

    chore: aggiunto .gitignore, rimossi node_modules e scratch dal tracking

--- scratch/check.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const content = fs.readFileSync('index.html', 'utf8'); const start = content.indexOf('deadlineScaricoTachigrafo'); console.log(content.substring(start - 300, start + 300));
